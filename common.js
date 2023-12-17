ccf7cc30fbe321954e8b48593573d64870feed8f
AircGroup <theartemimaev242007@gmail.com>
2023-12-17 23:26:27 +1100

    Version 0.7

--- common.js
+++ common.js
@@ -1,8 +1,13 @@
 $(".quote").animate({opacity: "0", margin: "0 0 100px 0"})
 $(".author").animate({opacity: "0", margin: "100px 0 0 0"})
 
+$('.logo').click(function(){
+	$('.menu').toggleClass('open-menu');
+	$('body').toggleClass('fixed-page');
+});
+
 $(document).ready(function(){
-  $(".owl-carousel").owlCarousel({items: 1, loop: true, nav: true, margin: 10, autoplay: true, autoplayTimeout: 4000, smartSpeed: 1000});
+	$(".owl-carousel").owlCarousel({items: 1, loop: true, nav: true, margin: 10, smartSpeed: 1000});
 });
 
 $(".author").animate({opacity: "1", margin: "10 0 0 0"}, 3000);
